b1714e36d9e62d338b160d3b10fc2552ba6c060c
James Frost <git@frost.cx>
2021-02-23 21:24:01 +0000

    Add all steps into shell script for single command processing.

--- process_raw_image.sh
+++ process_raw_image.sh
@@ -2,5 +2,11 @@
 
 OUT_FILE=$(basename -s .JPG $1).png
 
+# Cut out and straighten relavent section of spectra, then greyscale.
 convert "$1" -rotate -9.5 -shear 8x0 -crop 357x1110+2420+820 -grayscale Average "$OUT_FILE"
 
+# Make graph from processed image.
+python3 graph_spectra.py "$OUT_FILE"
+
+#clear up processed image.
+rm "$OUT_FILE"
